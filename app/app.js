b8faea580d3844a18f7f605b12169b5533cb6020
WestMecRyan <129911377+WestMecRyan@users.noreply.github.com>
2024-06-04 19:35:14 -0700

    added routes for /books

--- app/app.js
+++ app/app.js
@@ -1,31 +1,33 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var { sequelize } = require("./models"); // Import Sequelize instance
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var { sequelize } = require('./models'); // Import Sequelize instance
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  const error = new Error("Page Not Found");
+  const error = new Error('Page Not Found');
   error.status = 404;
   next(error);
 });
@@ -33,24 +35,24 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   err.status = err.status || 500;
-  err.message = err.message || "Internal Server Error";
+  err.message = err.message || 'Internal Server Error';
   console.error(`Status: ${err.status}, Message: ${err.message}`);
 
   if (err.status === 404) {
     res.status(404);
-    res.render("page-not-found", { error: err });
+    res.render('page-not-found', { error: err });
   } else {
     res.status(err.status);
-    res.render("error", { error: err });
+    res.render('error', { error: err });
   }
 });
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
     await sequelize.sync({ force: false }); // Set to true if you want to drop and recreate tables
-    console.log("Database synced successfully.");
+    console.log('Database synced successfully.');
 
     // Start the server after the database is synced
     const PORT = process.env.PORT || 3000;
@@ -58,7 +60,7 @@ app.use(function (err, req, res, next) {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error);
   }
 })();
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,15 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get('/', function (req, res, next) {
+  // res.render("index", { title: "Express" });
+  res.redirect('/books');
 });
 
 /* Route that throws an error for testing */
-router.get("/error", function (req, res, next) {
-  const error = new Error("Intentional Error");
+router.get('/error', function (req, res, next) {
+  const error = new Error('Intentional Error');
   error.status = 500;
   next(error);
 });
